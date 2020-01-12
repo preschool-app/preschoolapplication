1463821ac368213f61ffa3279bbd28dfb88e38dc
Robi Sorber <robisorber2000@gmail.com>
2020-01-12 12:38:39 +0800

    commiting to master

---
+++ components/FooterPrismic.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer">
+    <!-- <a href="http://minorisd.nl/?page_id=10" target="_blank" rel="noopener">
+      <img class="logo" src="../assets/img/MinorISD.png" alt="Prismic">
+    </a> -->
+    <p>&copy; 2019.  Made with &hearts; by Olaf Stumpf & Robi Sorber</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterPrismic'
+}
+</script>
+
+<style lang="sass" scoped>
+.footer
+  padding: 20px 25px
+  max-width: 700px
+  margin: 0 auto
+  text-align: center
+  border-top: 1px solid #DADADA
+  font-size: 13px
+
+.logo
+  width: 130px
+</style>
